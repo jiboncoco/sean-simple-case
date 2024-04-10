06c096d5afa9ced639727e249a0589249d1918c6
Achmad fauzi <achmadfauzi@live.com>
2024-04-10 19:57:30 +0700

    create get and post successfully

--- astro-supabase/backend/server.js
+++ astro-supabase/backend/server.js
@@ -1,10 +1,36 @@
 const express = require('express')
-const app = express()
-const PORT = 3211 || process.env.PORT
+const supabase = require('@supabase/supabase-js')
 
-app.get('/', (req, res) => {
-    console.log("API first endpoint successfully accessed")
-    res.send("this is the first endpoint")
+const app = express()
+app.use(express.json())
+
+const PORT = 3211 || process.env.PORT
+const SUPABASE_URL = "https://unciirpmrcadghxvjhas.supabase.co"
+const SUPABASE_SERVICE_ROLE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuY2lpcnBtcmNhZGdoeHZqaGFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTIwMzY5NCwiZXhwIjoyMDI2Nzc5Njk0fQ.QJGBbSs5mZip-4_inCbzCDJ_un0bBNM800dkowgpIY8"
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
+
+app.get('/', async(req, res) => {
+
+    const getBlog = await db.from("blog").select()
+
+    // console.log(getBlog)
+    // console.log("API first endpoint successfully accessed")
+    // res.send("this is the first endpoint")
+
+    res.json({
+        getBlog
+    })
+})
+
+app.post('/', async(req, res) => {
+    const { title, description } = req.body
+    const createPost = await db.from("blog").insert({ title, description})
+
+    // console.log(title, description)
+    console.log(createPost)
+
+    res.json({createPost})
 })
 
 app.listen(PORT, () => {
